35ba03d7ca9875285f4f4b127127f25d69d2e869
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-09-05 21:16:03 -0600

    create then delete temporary directory

--- src/aspm.cpp
+++ src/aspm.cpp
@@ -1,10 +1,20 @@
 #include <cstdlib>
+#include <cstdio>
 
+#include "lib/directory.hpp"
 #include "lib/logger.hpp"
 
 int main(void){
     aspm::Logger::set_log_display_level(aspm::LogLevel::Verbose);
-    aspm::Logger::log(aspm::LogLevel::Verbose, "main", "Hello World!");
+    
+    std::puts("Hello World");
+
+    auto directory = aspm::create_temporary_directory();
+
+    if(directory){
+        aspm::Logger::log(aspm::LogLevel::Verbose, "main", directory.value().c_str());
+        aspm::delete_directory(directory.value());
+    }
 
     return EXIT_SUCCESS;
 }
