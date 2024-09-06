976d5b39b3a63e5556cb2db67c02024c117833b4
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-09-05 21:14:47 -0600

    create directory methods

---
+++ src/lib/directory.cpp
@@ -0,0 +1,90 @@
+#include "directory.hpp"
+
+#include <unistd.h>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <ftw.h>
+
+#include "logger.hpp"
+
+using aspm::path;
+
+std::optional<path> aspm::create_temporary_directory(path base){
+    // create directory template in the format wanted by mkdtemp
+    base.append("aspm.XXXXXX");
+    std::string templatestring = base.string();
+
+    // avoid undefined behavior
+    //  - const_cast<char*>(templatestring.c_str()) would probably work but is undefined
+    char *directory = new char[templatestring.size() + 1];
+    strcpy(directory, templatestring.c_str());
+
+    if(mkdtemp(directory) == NULL){
+        aspm::Logger::log(aspm::LogLevel::Error, "tmpdir", std::strerror(errno));
+        return {};
+    }
+    
+    path result = directory;
+    delete[] directory;
+    return result;
+}
+
+int aspm::delete_directory(path directory){
+    const auto update_permissions = [](const char *fpath, const struct stat*, int, FTW*) -> int {
+        // take ownership of the file
+        if(chown(fpath, getuid(), getgid())){
+            // if we can not take ownership, try leaving permissions as they were
+            // note: returning FTW_CONTINUE is not really needed unless FTW_ACTIONRETVAL is set
+            return FTW_CONTINUE;
+        }
+
+        // change the permissions to allow read/write from owner
+        if(chmod(fpath, S_IRUSR | S_IRUSR)){
+            aspm::Logger::log(aspm::LogLevel::Error, "deldir", std::strerror(errno));
+        }
+
+        // note: returning FTW_CONTINUE is not really needed unless FTW_ACTIONRETVAL is set
+        return FTW_CONTINUE;
+    };
+
+    const auto delete_path = [](const char *fpath, const struct stat*, int typeflag, FTW*) -> int {
+        // delete the path
+        switch(typeflag){
+            case FTW_F: // path is a file
+                // try to delete
+                // fail is ok ish
+                if(unlink(fpath)){
+                    aspm::Logger::log(aspm::LogLevel::Error, "deldir", std::strerror(errno));
+                }
+
+                break;
+            
+            case FTW_D: // path is a directory
+            case FTW_DP:
+                // try to delete
+                // fail is ok ish
+                if(rmdir(fpath)){
+                    aspm::Logger::log(aspm::LogLevel::Error, "deldir", std::strerror(errno));
+                }
+
+                break;
+        }
+
+        // note: returning FTW_CONTINUE is not really needed unless FTW_ACTIONRETVAL is set
+        // but I need to return something anyways so why not
+        return FTW_CONTINUE;
+    };
+
+    // update all permissions (pre order, ignore mounts, do not follow symlinks)
+    if(nftw(directory.c_str(), update_permissions, -1, FTW_MOUNT | FTW_PHYS)){
+        return -1;
+    }
+
+    // delete all files and directories (post order, ignore mounts, do not follow symlinks)
+    if(nftw(directory.c_str(), delete_path, -1, FTW_DEPTH | FTW_MOUNT | FTW_PHYS)){
+        return -1;
+    }
+
+    return errno ? -1 : 0;
+}
---
+++ src/lib/directory.hpp
@@ -0,0 +1,21 @@
+#ifndef ASPM_LIB_TMPDIR_HPP
+#define ASPM_LIB_TMPDIR_HPP
+
+#include <filesystem>
+#include <optional>
+#include <cerrno>
+
+namespace aspm {
+
+using path = std::filesystem::path;
+
+// attempts to create a temporary directory in the provided base directory
+std::optional<path> create_temporary_directory(path base = "/tmp");
+
+// attempts to delete the directory at the provided path
+// on success returns zero, on failure returns -1
+int delete_directory(path);
+
+};
+
+#endif
