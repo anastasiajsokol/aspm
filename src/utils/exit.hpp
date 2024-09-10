5923851a3187032690be4cac641ba17de4a2e94d
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-09-10 10:49:38 -0600

    comment, add quote, and add exit_type

--- src/utils/exit.hpp
+++ src/utils/exit.hpp
@@ -1,14 +1,31 @@
+/**
+ *  Все счастливые семьи похожи друг на друга,
+ *  каждая несчастливая семья несчастлива по-своему.
+ *      - Лев Николаевич Толстой
+**/
+
 #ifndef ASPM_UTILS_EXIT_HPP
 #define ASPM_UTILS_EXIT_HPP
 
 namespace utils {
 
+using exit_type = unsigned char;
+
 namespace exit {
 
-constexpr unsigned char SUCCESS = 0;
-constexpr unsigned char FAILURE = 1;
-constexpr unsigned char INVALID_ARGUMENTS = 2;
-constexpr unsigned char INSUFFICENT_PERMISSIONS = 3;
+// replaces EXIT_SUCCESS, show that all is ok
+constexpr exit_type SUCCESS = 0;
+
+// generic failure, prefer using a specific code (or making a new one)
+constexpr exit_type FAILURE = 1;
+
+// as an exit code this means command line arguments
+// as a return code may mean function arguments
+constexpr exit_type INVALID_ARGUMENTS = 2;
+
+// insufficent system permissions to perform an action
+// should be returned based on errno
+constexpr exit_type INSUFFICENT_PERMISSIONS = 3;
 
 };
 
