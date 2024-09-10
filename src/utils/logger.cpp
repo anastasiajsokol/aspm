5a3bfa8ae9b1f925d33a36bcbb38cfbb16d8b268
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-09-10 10:41:54 -0600

    comment logging utility

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -9,25 +9,49 @@ using utils::Logger;
 
 void Logger::verbose(zstring format, ...){
     if(verbosity == Logger::Verbosity::Verbose){
+        // start variadic argument list
         va_list args;
         va_start(args, format);
+
+        // standard program id header
         std::fputs("aspm: ", stdout);
+        
+        // print to stdout
         std::vprintf(format, args);
+
+        // final newline
+        // a newline in format would be bad form for the syslog functions
         std::putc('\n', stdout);
+
+        // make sure to cleanup variadic argument list
         va_end(args);
     }
 }
 
 void Logger::warning(zstring format, ...){
+    // warnings may be turned off by setting verbosity to silent
     if(verbosity != Logger::Verbosity::Silent){
+        // start variadic argument list
         va_list args;
         va_start(args, format);
+
+        // print standard prefix with warning tag (internationalized)
+        // TODO: option to turn off internationalization of logging tags
         std::fprintf(stderr, "aspm: (%s) ", gettext("warning"));
+
+        // print the provided message
         std::vfprintf(stderr, format, args);
+        
+        // end with a final newline
+        // bad form to place newlines in syslog so this encourages not placing them in format
         std::putc('\n', stderr);
+
+        // cleanup variadic arguments
         va_end(args);
 
         if(system_logging){
+            // log to syslog if system logging is enabled
+            // making sure to restart the variadic list
             va_start(args, format);
             vsyslog(LOG_WARNING, format, args);
             va_end(args);
@@ -36,15 +60,29 @@ void Logger::warning(zstring format, ...){
 }
 
 void Logger::error(zstring format, ...){
+    // errors may be turned off by setting verbosity to silent
     if(verbosity != Logger::Verbosity::Silent){
+        // initialize variadic arguments
         va_list args;
         va_start(args, format);
+
+        // print aspm program header with error tag
+        // TODO: option to turn off internationalization of logging tags
         std::fprintf(stderr, "aspm: (%s) ", gettext("error"));
+
+        // print the message
         std::vfprintf(stderr, format, args);
+
+        // end with a newline
+        // bad form to place newline in format for syslog
         std::putc('\n', stderr);
+
+        // cleanup varadic argument list
         va_end(args);
 
         if(system_logging){
+            // log to system if system logging is enabled
+            // make sure to restart variadic argument list
             va_start(args, format);
             vsyslog(LOG_ERR, format, args);
             va_end(args);
@@ -53,10 +91,15 @@ void Logger::error(zstring format, ...){
 }
 
 void Logger::print(zstring format, ...){
+    // only print if not silent
     if(verbosity != Logger::Verbosity::Silent){
         va_list args;
         va_start(args, format);
+
+        // here format should probably end in a newline
+        // not really a logging function
         std::vprintf(format, args);
+        
         va_end(args);
     }
 }
--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -10,12 +10,47 @@ using zstring = const char*;
 
 class Logger {        
     public:
+        /**
+         *   Displays the message with standard program id if verbosity is set to verbose
+         * 
+         *  @param format will be passed to a variation of std::printf
+         *  @note does not print to system log even if system logging is enabled
+         *  @note ideally format should not include any newlines
+        **/
         static void verbose(zstring format, ...);
+
+        /**
+         *   Prints a warning to stderr with an prefix of 'aspm: (warning)' unless the verbosity is set to silent
+         * 
+         *  @param format will be passed to a variation of std::printf
+         *  @note prints to the system log if system logging is enabled
+         *  @note ideally format should not include any newlines
+        **/
         static void warning(zstring format, ...);
+
+        /**
+         *   Prints an error to stderr with an prefix of 'aspm: (error)' unless the verbosity is set to silent
+         * 
+         *  @param format will be passed to a variation of std::printf
+         *  @note prints to the system log if system logging is enabled
+         *  @note ideally format should not include any newlines
+        **/
         static void error(zstring format, ...);
 
+        /**
+         *   Prints to console as-is unless verbosity is set to silent
+         * 
+         *  @param format will be passed to a variation of std::printf
+         *  @note does not print any additional content
+         *  @note should probably at least have an ending newline
+        **/
         static void print(zstring format, ...);
 
+        /**
+         *  Opens a handle to the system log and tells the warning and error functions to use it
+         *  
+         *  @warning must be called before entering a chroot environment
+        **/
         static void enable_system_logging();
         
         enum class Verbosity {
@@ -24,6 +59,13 @@ class Logger {
             Verbose
         };
 
+        /**
+         *  Sets the verbosity level for logging functions
+         * 
+         *  @note silent: do not print anything ever
+         *  @note brief: (default) do not print verbose messages
+         *  @note verbose: print everything
+        **/
         static void set_verbosity(Verbosity);
 
     private:
